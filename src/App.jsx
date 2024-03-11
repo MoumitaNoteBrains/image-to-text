76697d19aad2234a8b5a68efb016d4d046eaa522
Moumita das <moumitad@notebrains.com>
2024-03-11 15:48:21 +0530

    adding text -to img code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { createWorker } from "tesseract.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [textImage, setTextImage] = useState("");
+
+  const handleChange = (e) => {
+    setSelectedImage(e.target.files[0]);
+  };
+
+  const convertImgToText = async () => {
+    const worker = await createWorker("eng");
+    const ret = await worker.recognize(selectedImage);
+    console.log(ret?.data.text);
+    setTextImage(ret?.data?.text);
+    await worker.terminate();
+  };
+
+  useEffect(() => {
+    convertImgToText();
+  }, [selectedImage]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label htmlFor="upload">Upload image</label>
+        <input
+          type="file"
+          id="upload"
+          accept="image/*"
+          onChange={handleChange}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {selectedImage && (
+          <div>
+            <img
+              style={{ width: "420px" }}
+              src={URL.createObjectURL(selectedImage)}
+              alt="Uploaded"
+            />
+          </div>
+        )}
+        {textImage && <div>{textImage}</div>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
